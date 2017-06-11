c95daf106613ada12b810980cdba666c7cc1791a
Emmanuel <Emmanuel@DESKTOP-ITD9VJ9>
2017-06-11 15:22:56 -0300

    estilos situacion.php

--- index.php
+++ index.php
@@ -13,26 +13,26 @@ and open the template in the editor.
     </head>
     <body>
         <nav class="navbar navbar-default"> 
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>                        
-                        </button>
-                        <a class="navbar-brand" href="#">Sistema Acreditacion de Carreras</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Inicio</a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        </ul>
-                    </div>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">Sistema Acreditacion de Carreras</a>
                 </div>
-            </nav>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Inicio</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div class="container">
             <div class="col-lg-12">
                 <h3>Consulta de Indicadores por Categoria</h3>
--- situacion.php
+++ situacion.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="public/css/situacion.css" rel="stylesheet" type="text/css"/>
         <title>Situacion General Actual</title>
     </head>
     <body>
-        <div> <!-- cuerpo de indicadores generales-->
-            <div><h1>Sistema Indicadores de Acreditaci&oacute;n</h1></div>
-            <div>SIA</div>
-            <div>Bienvenido:</div>
-            <div>Situaci&oacute;n actual de los indicadores </div>
-            <div>Docentes con capacitaciones</div>
-            <div>Docentes con Postgrados</div>
-            <div>Resultados de evaluaci&oacute;n docente</div>
+        <nav class="navbar navbar-default"> 
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">Sistema Acreditacion de Carreras</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Inicio</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container"> <!-- cuerpo de indicadores generales-->
+            <div class="col-lg-12"><h1>Sistema Indicadores de Acreditaci&oacute;n</h1></div>
+            <div class="col-lg-2">SIA</div>
+            <div class="col-lg-2">Bienvenido:</div>
+            <div class="col-lg-2">Situaci&oacute;n actual de los indicadores </div>
+            <div class="col-lg-2">Docentes con capacitaciones</div>
+            <div class="col-lg-2">Docentes con Postgrados</div>
+            <div class="col-lg-2">Resultados de evaluaci&oacute;n docente</div>
         </div>
-         <footer><h5>Powered by ROS 2017</h5></footer>
+        <footer><h5>Powered by ROS 2017</h5></footer>
         <?php
         // put your code here
         ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
