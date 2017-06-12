eb63245290340c947d77cd4bb0df14983cc1ca20
Cristian! <cristhianferni@gmail.com>
2017-06-12 17:07:21 -0300

    grafico añadido

--- situacion.php
+++ situacion.php
@@ -46,8 +46,9 @@
                 <br>
                 <br>
             </div>
+            <footer><h5>Powered by ROS 2017</h5></footer> 
         </div>
-        <footer><h5>Powered by ROS 2017</h5></footer>
+      
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
